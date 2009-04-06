5852f3a3aa9ef2793db5cef64633c2914c791c6d
Yushi Nakai <yushi.nakai@gmail.com>
2009-04-06 23:02:27 +0900

    - change interface(get,set -> enqueue, dequeue) - add blocking/nonblocking interface(dequeue, dequeue_nb)

--- src/libbbevqueue/memd_client.cpp
+++ src/libbbevqueue/memd_client.cpp
@@ -6,11 +6,11 @@ int main(){
   BeatBoard::ProtobufQueueMemcached queue;
 
   for(int i = 0; i < 1; i++){
-    queue.set(string("koko ni protobuf no data ireruyo-"));
+    queue.enqueue(string("koko ni protobuf no data ireruyo-"));
   }
 
   for(int i = 0; i < 1; i++){
-    string *value = queue.get();
+    string *value = queue.dequeue();
     if(value != NULL){
       cout << *value << endl;
       delete value;
