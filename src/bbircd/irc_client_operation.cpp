6bb4d645a997e51a7ea09264136c7214defc9e71
Yushi Nakai <yushi.nakai@gmail.com>
2009-08-10 21:35:11 +0900

    add debug code

--- src/bbircd/irc_client_operation.cpp
+++ src/bbircd/irc_client_operation.cpp
@@ -1,15 +1,24 @@
 #include "irc_client_operation.h"
-
+using namespace std;
 BeatBoard::IRCClientOperation::IRCClientOperation(){
 }
 
 BeatBoard::IRCClientOperation::~IRCClientOperation(){
 }
 
-void BeatBoard::IRCClientOperation::setIRCConnection(std::string nick, IRCConnection* conn){
+void BeatBoard::IRCClientOperation::setIRCConnection(string nick, IRCConnection* conn){
   this->ircConnections[nick] = conn;
 }
 
-BeatBoard::IRCConnection* BeatBoard::IRCClientOperation::getIRCConnection(std::string nick){
-  return this->ircConnections[nick];
+BeatBoard::IRCConnection* BeatBoard::IRCClientOperation::getIRCConnection(string nick){
+  IRCConnection* conn = this->ircConnections[nick];
+  if(conn == NULL){
+    cout << "null detected!!!!!!!!!!!!!!!!!!" << endl;
+    map<string, IRCConnection*>::iterator i = this->ircConnections.begin();
+    while(i != this->ircConnections.end()){
+      cout << i->first << ": " << sizeof(i->first) << endl;
+      i++;
+    }
+  }
+  return conn;
 }
