45c4272d5a4db480b63353b6f7c34011168ee3d8
Yushi Nakai <yushi.nakai@gmail.com>
2009-08-16 21:25:10 +0900

    separate html and js

--- src/bbui/bbui.js
+++ src/bbui/bbui.js
@@ -7,27 +7,27 @@ var debug = 0;
 $.ajaxSetup({'timeout': 300000});
 
 function connectServer(){
-    var server = $("#server").val();
-    var nick = $("#nick").val();
-    var port = $("#port").val();
+    var server = $('#server').val();
+    var nick = $('#nick').val();
+    var port = $('#port').val();
     connect(server, nick, port);
     return false;
 }
 
 function joinChannel(elem){
-    var channel = $("#channel").val();
-    var nick = $("#nick").val();
+    var channel = $('#channel').val();
+    var nick = $('#nick').val();
     join(channel, nick);
     return false;
 }
 
 function sendMessage(elem){
-    var target = $("#target").val();
-    var message = $("#message").val();
+    var target = $('#target').val();
+    var message = $('#message').val();
     
     if(message.length != 0){
         privmsg(target, message, nick);
-        $("#message").val("");
+        $('#message').val('');
     }
     return false;
 }
@@ -38,7 +38,7 @@ function checkLoader(){
         readMessage(nick);
         
     }
-    update_debuginfo("checkLoader");
+    update_debuginfo('checkLoader');
 }
 
 
@@ -47,25 +47,21 @@ function connect(server, nickname, port){
     $.cookie('nick',nickname);
     $.cookie('server',server);
     $.cookie('port',port);
-    debug_log("connect req");
-    $.post("/api/CONNECT",
+    debug_log('connect req');
+    $.post('/api/CONNECT',
            {
                'server': server,
-               "nick" : nick ,
-               "port" : port
+               'nick' : nick ,
+               'port' : port
            },
            function(data){
-               debug_log("connect res");
-               eval("obj=" + data);
-               $("#status").html(obj["reason"]);
-               if(obj["status"].match('^OK$')){
-                   $("#connect").toggle();
-                   $("#join_channel").append(
-                       '<form  id="join_form" onsubmit="javascript:return joinChannel();">' +
-                           '<input id="nick" type="hidden" value="' + nick + '"></input>' +
-                           'channel<input id="channel" type="text" value="#yushi"></input>' + 
-                           '</form>'
-                   );
+               debug_log('connect res');
+               eval('obj=' + data);
+               $('#status').html(obj['reason']);
+               if(obj['status'].match('^OK$')){
+                   $('#connect').toggle();
+                   $('#join_channel').load('join_channel.html');
+                   $('#nick').val(nick);
                }
            });
     
@@ -75,34 +71,30 @@ function join(channel, nick){
     var url = '/api/JOIN';
     $.cookie('channel',channel);
     active_channel = channel;
-    debug_log("join req");
+    debug_log('join req');
     $.post(url,
            {
                'channel':channel, //escape(channel),
                'nick' : nick,
            },
            function(data){
-               debug_log("join res");
-               eval("obj=" + data);
-               $("#status").html(obj["reason"]);
-               if(obj["status"].match('^OK$')){
-                   $("#send_message").append(
-                       '<form id="message_form" onsubmit="javascript:return sendMessage(this);">' +
-                           '<input id="nick" type="hidden" value="' + nick + '"></input>' +
-                           '<input id="target" type="hidden" value="' + channel + '"></input>' +
-                           'message<input id="message" type="text" ></input>' +
-                           '</form>'
-                   );
-                   $("#join_channel").toggle();
-                   $("#head").append(channel);
+               debug_log('join res');
+               eval('obj=' + data);
+               $('#status').html(obj['reason']);
+               if(obj['status'].match('^OK$')){
+                   $('#send_message').load('send_message.html');
+                   $('#nick').val(nick);
+                   $('#target').val(channel);
+                   $('#join_channel').hide();
+                   $('#head').append(channel);
                    setInterval(checkLoader, 1000);
                }
            });
 }
 
 function privmsg(target, message, nick){
-    var url = "/api/SPEAK";
-    debug_log("privmsg req");
+    var url = '/api/SPEAK';
+    debug_log('privmsg req');
     $.post(url,
            {
                'message':message,
@@ -110,43 +102,43 @@ function privmsg(target, message, nick){
                'nick':nick,
            },
            function(data){
-               debug_log("privmsg res");
-               $("#messages").append(nick + ":" + replace_centity_ref(message) + "<br />");
+               debug_log('privmsg res');
+               $('#messages').append(nick + ':' + replace_centity_ref(message) + '<br />');
                window.scrollBy( 0, screen.height );
            });
 }
 
 function readMessage(nick){
-    var url = "/api/READ";
-    debug_log("read req");
+    var url = '/api/READ';
+    debug_log('read req');
     $.ajax({
                'type': 'POST',
                'url': url,
                'data': {'nickname':nick},
                cache: false,
                success: function(data){
-                   debug_log("read response succeess");
+                   debug_log('read response succeess');
                    try{
-                       eval("received=" + data);
+                       eval('received=' + data);
                        var messages = received[active_channel];
                        for(var i = 0; i < messages.length; i+=2){
-                           var match_result = messages[i].match("(.+)!.*");
+                           var match_result = messages[i].match('(.+)!.*');
                            if(match_result){
-                               $("#messages").append(match_result[1] + ": ");
-                               $("#messages").append(replace_centity_ref(messages[i+1]) + "<br />");
+                               $('#messages').append(match_result[1] + ': ');
+                               $('#messages').append(replace_centity_ref(messages[i+1]) + '<br />');
                            }else{
-                               $("#status").html("JOINERS: " + messages[i+1]);
+                               $('#status').html('JOINERS: ' + messages[i+1]);
                            }
                        }
                    }catch(e){
-                       alert("error in read received")
+                       alert('error in read received')
                    }
                    loading = 0;
                    window.scrollBy( 0, screen.height );
-                   update_debuginfo("read success");
+                   update_debuginfo('read success');
                },
                error: function(XMLHttpRequest, textStatus, errorThrown){
-                   debug_log("read response error");
+                   debug_log('read response error');
                    loading = 0;
                }
            });
@@ -160,7 +152,7 @@ function getopt(){
     }
     var args_array = args_string.split('&');
     for(var i = 0; i < args_array.length; i++){
-        var arg = args_array[i].split("=");
+        var arg = args_array[i].split('=');
         args[arg[0]] = arg[1];
     }
     return args;
@@ -169,7 +161,7 @@ function getopt(){
 function replace_centity_ref(message) {
     var centity_ref_array = new Array();
     centity_ref_array.push(new Array(RegExp('&', 'g'), '&amp;'));
-    centity_ref_array.push(new Array(RegExp('"', 'g'), '&quot;'));
+    centity_ref_array.push(new Array(RegExp('\'', 'g'), '&quot;'));
     centity_ref_array.push(new Array(RegExp('<', 'g'), '&lt;'));
     centity_ref_array.push(new Array(RegExp('>', 'g'), '&gt;'));
     for(var i = 0; i < centity_ref_array.length; i++) {
@@ -183,6 +175,10 @@ function init(){
     var args = getopt();
     if(args['debug']){
         debug = new Object();
+    }else{
+        $('#debug').hide();
+        $('#debug_menu').hide();
+        $('#debug_log').hide();
     }
 
     nick = $.cookie('nick');
@@ -192,28 +188,35 @@ function init(){
     if($.cookie('channel')){
         join($.cookie('channel'), $.cookie('nick'));
     }
-    update_debuginfo("init");
-    debug_log("init");
+    update_debuginfo('init');
+    debug_log('init');
 }
 
 function update_debuginfo(from){
     if(debug){
-        var debug_text = "";
-        debug_text += "loading = " + loading + "<br />";
-        debug_text += "last updated = " + (new Date).toLocaleString() + "<br />";
+        var debug_text = '';
+        debug_text += 'loading = ' + loading + '<br />';
+        debug_text += 'last updated = ' + (new Date).toLocaleString() + '<br />';
         if(from){
-            debug_text += "updated by " + from + "<br />";
+            debug_text += 'updated by ' + from + '<br />';
         }
-        $("#debug").html(debug_text);
+        $('#debug').html(debug_text);
     }
     return;
 }
 
 function debug_log(message){
     if(debug){
-        var debug_text = (new Date).toLocaleString() + ":  " + message + "<br />";
-        $("#debug_log").append(debug_text);
+        var debug_text = (new Date).toLocaleString() + ':  ' + message + '<br />';
+        $('#debug_log').append(debug_text);
     }
     return;
+}
+
+function delete_cookie(){
+    $.cookie('server',null);
+    $.cookie('nick', null);
+    $.cookie('port', null);
+    $.cookie('channel', null);
 }
 init();
