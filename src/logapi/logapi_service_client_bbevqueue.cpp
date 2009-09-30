84edf61b8eacf6ae81294c8ea9b8ba340cad56ee
Hiroki Kuzuno <kuzuno@gmail.com>
2009-10-01 03:45:51 +0900

    remove timestamp field from message map key

--- src/logapi/logapi_service_client_bbevqueue.cpp
+++ src/logapi/logapi_service_client_bbevqueue.cpp
@@ -72,7 +72,7 @@ BeatBoard::LogApiServiceClientBbevqueue::dequeueLogData()
     request.ParseFromString(*value);
 
     std::string identifier = removeIPaddressFromIdentifier();
-    std::string key = request.channel() + request.time() + identifier + request.message();
+    std::string key = request.channel() + identifier + request.message();
     std::cerr << "key: " << key << std::endl;
     
     if (checkMessageDuplication(key))
