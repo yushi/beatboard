cfcdbf39ec3579a5bdaf90be03bc68fa513ee95e
Yushi Nakai <yushi@renge.(none)>
2009-11-18 21:32:45 +0900

    fixed url regex without &nbsp

--- src/bbui/bbui.js
+++ src/bbui/bbui.js
@@ -203,7 +203,7 @@ function extractLink(str, channel){
     imgRegex.compile(/https?:\/\/\S+\.(jpe?g|png|gif|bmp)/);
 
     var urlRegex = new RegExp("");
-    urlRegex.compile(/https?:\/\/[0-9a-zA-Z\-\+\/\;\!\*\(\),\.\_\?=#%~]+/);
+    urlRegex.compile(/(https?:\/\/(?:(?!\&nbsp).)*)(.*)/);
     var match_result = str.match(urlRegex);
     if(match_result){
         //extract videos
@@ -226,18 +226,19 @@ function extractLink(str, channel){
         if(no_refferer){
             //IE not supported
 	    var html = '<html><head><script type="text/javascript"><!--\n'
-	        + 'document.write(\'<meta http-equiv="refresh" content="0;url='+match_result[0]+'">\');'
+	        + 'document.write(\'<meta http-equiv="refresh" content="0;url='+match_result[1]+'">\');'
 	        + '// --><'+'/script></head><body></body></html>';
             str = str.replace(
                 urlRegex,
                 "<a target=\"_blank\" href=\"data:text/html;charset=utf-8,'" 
                     + encodeURIComponent(html) 
                     + "\" >" 
-                    + match_result[0] 
+                    + match_result[1] 
                     + "</a>"
+                    + match_result[2]
             );
         }else{
-            str = str.replace(urlRegex, "<a target=\"_blank\" href=\"" + match_result[0] + "\" >" + match_result[0] + "</a>");
+            str = str.replace(urlRegex, "<a target=\"_blank\" href=\"" + match_result[1] + "\" >" + match_result[1] + "</a>" + match_result[2]);
         }
     }
     return str;
