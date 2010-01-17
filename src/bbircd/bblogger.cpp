91fa727d277de571f26c2f7cd14102d3f1f168b7
Yushi Nakai <yushi@renge.(none)>
2010-01-17 20:54:32 +0900

    change log format

--- src/bbircd/bblogger.cpp
+++ src/bbircd/bblogger.cpp
@@ -1,20 +1,34 @@
 #include "bblogger.h"
 
-BeatBoard::BBLogger::BBLogger(){
+BeatBoard::BBLogger::BBLogger() {
   return;
 }
 
-void BeatBoard::BBLogger::debug(const std::string message){
-  this->printLog( "DEBUG", message);
+void BeatBoard::BBLogger::debug(const string message) {
+  this->printLog("DEBUG", message);
   return;
 }
 
-void BeatBoard::BBLogger::info(const std::string message){
-  this->printLog( "INFO", message);
+void BeatBoard::BBLogger::info(const string message) {
+  this->printLog("INFO", message);
   return;
 }
 
-void BeatBoard::BBLogger::printLog(const std::string level, const std::string message){
-  std::cout << level.data() << ": " << message.data() << std::endl;
+void BeatBoard::BBLogger::printLog(const string level, const string message) {
+  cout << this->get_current_time_str() << " ";
+  cout << level.data() << " ";
+  cout << message.data() << endl;
   return;
 }
+
+string BeatBoard::BBLogger::get_current_time_str() {
+  time_t t = time(NULL);
+  struct tm *tm;
+  char str[81];
+
+  tm = localtime(&t);
+  strftime(str, sizeof(str), "[%Y-%m-%d %H:%I:%S]", tm);
+
+  return string(str);
+}
+
