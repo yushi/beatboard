4a5c2fc55c8b688698ae520e86738b3555782cb0
Yushi <yushi.nakai@gmail.com>
2010-04-02 00:38:13 +0900

    support commandline options

--- src/bbircd/bbircd.cpp
+++ src/bbircd/bbircd.cpp
@@ -5,12 +5,12 @@ void sig_handler_SIGPIPE(int sig) {
   logger.debug("SIGPIPE received");
 }
 
-void BeatBoard::BBIRCD::Daemon::setUp() {
+void BeatBoard::BBIRCD::Daemon::setUp(char* addr, int port) {
   BeatBoard::BBLogger logger = BeatBoard::BBLogger::getInstance();
 
   try {
     this->httpd = new HTTPAPIServer();
-    this->httpd->setUp(http_api_server_addr, http_api_server_port);
+    this->httpd->setUp(addr, port);
   } catch (BeatBoard::Exception& error) {
     logger.debug(error.message.data());
     std::cerr << "daemon start failed.\n";
@@ -34,15 +34,35 @@ void unexpected_exception_handler(void) {
   logger.debug("unknown exception throwed");
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char* argv[]) {
+  int result;
+  int port = 8000;
+  char default_addr[] = "0.0.0.0";
+  char* addr = default_addr;
+  
+  while((result=getopt(argc,argv,"ha:p:"))!=-1){
+    switch(result){
+    case 'a':
+      addr = (char*)calloc(1, sizeof(char) * strlen(optarg) + 1);
+      strncpy(addr,optarg,strlen(optarg));
+      break;
+    case 'p':
+      port = atoi(optarg);
+      break;
+    case 'h':
+      std::cout << "bbircd [-a <address>] [-p <port>]" << endl;
+      exit(-1);
+      break;
+    }
+    
+  }
   BeatBoard::BBLogger logger = BeatBoard::BBLogger::getInstance();
   logger.info("bbircd started");
   std::set_unexpected(unexpected_exception_handler);
 
   signal(SIGPIPE , sig_handler_SIGPIPE);
   BeatBoard::BBIRCD::Daemon bbircd;
-  bbircd.setUp();
+  bbircd.setUp(addr,port);
   bbircd.startService();
-
   return 0;
 }
--- src/bbircd/bbircd.h
+++ src/bbircd/bbircd.h
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <bb_exception.h>
 #include "bblogger.h"
 #include <pthread.h>
@@ -6,15 +7,12 @@
 #include "irc_client_operation.h"
 #include "http_api_server.h"
 
-char http_api_server_addr[] = "0.0.0.0";
-int http_api_server_port = 8000;
-
 namespace BeatBoard{
   namespace BBIRCD{
     class Daemon{
     public:
       void startService();
-      void setUp();
+      void setUp(char* addr, int port);
       IRCClientOperation ircClients;
     private:
       HTTPAPIServer *httpd;
